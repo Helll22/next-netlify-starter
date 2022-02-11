a9cbae619171c76c19199504594108749effac0c
Helll22 <99483992+Helll22@users.noreply.github.com>
2022-02-11 18:36:36 +0530

    Index.js
    Bbb

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container" color="red">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to Our hiiiiii app!" />
+        <Header title="Welcome to Our hiiiiii app!" / >
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
